28ad1072b056570ab46a0deb443f3f07ff964632
OzanKiral <65554792+OzanKiral@users.noreply.github.com>
2020-10-05 14:05:37 +0300

    Add project files.

---
+++ StokTakip/Giris.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StokTakip
+{
+    public partial class Giris : Form
+    {
+        public Giris()
+        {
+            InitializeComponent();
+        }
+
+        private void Giris_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
